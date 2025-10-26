cd7eb2af7f900000a3a9c1b29610fbf10df9fcdb
Valeria Tsabina <vvvtsabina@gmail.com>
2025-10-26 03:20:12 +0300

    detection script ready

--- headliners_CXRForeignViewer/scripts/prediction.py
+++ headliners_CXRForeignViewer/scripts/prediction.py
@@ -1,4 +1,11 @@
-#imports
+from ..lib.model_mapper import YoloViewer
 
-if __name__ == "__main__":
-    pass
+def main():
+    data = input("Введите путь к папке с файлами DICOM: ")
+
+    if not data or len(data) == 0:
+        data = 'headliners_CXRForeignViewer/demo_files'
+    print("Current data directory: ", data)
+
+    model = YoloViewer(trained=True)
+    model.predict(data, 'output')
---
+++ main.py
@@ -0,0 +1,5 @@
+from headliners_CXRForeignViewer.scripts.prediction import main as prediction_script
+
+if __name__ == "__main__":
+    prediction_script()
+    
